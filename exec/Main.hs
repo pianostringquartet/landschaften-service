43bed83925007cfc5025a2c31bbb0249d58f158d
pianostringquartet <cjc500@nyu.edu>
2019-10-08 16:18:15 +0200

    wip

--- exec/Main.hs
+++ exec/Main.hs
@@ -10,6 +10,12 @@ import Servant.Client
 import Servant
 import BuildQuery
 
+import qualified Data.ByteString                  as BS
+import System.Environment
+
+import           Data.ByteString.UTF8             (fromString)
+--type DBConnectionString = BS.ByteString
+
 getArtists :: ClientM ArtistsResponse
 getConcepts :: ClientM ConceptNamesResponse
 getPaintings :: ClientM PaintingsResponse
@@ -19,6 +25,8 @@ getArtists :<|> getConcepts :<|> getPaintings :<|> queryPaintings = client api
 
 main :: IO ()
 main = do
-  let connStr = "postgresql://localhost:5432/bilder"
-  pool <- initConnectionPool connStr
-  runApp pool
+--  let connStr = "postgresql://localhost:5432/bilder"
+  portNumber <- getEnv "PORT"
+  connStr <- getEnv "DB_URL"
+  pool <- initConnectionPool (fromString connStr)
+  runApp (read portNumber) pool
